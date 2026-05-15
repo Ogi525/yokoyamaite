d1c18ab6a58cacec460e6b746918610b6aa8388b
横山凜 <yyoko02212003@gmail.com>
2026-05-15 12:18:05 +0900

    ログイン画面遷移を実装（怪しい）

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,11 +1,14 @@
 package com.example.demo.controller;
 
+import jakarta.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.example.demo.entity.User;
 import com.example.demo.form.UserForm;
 import com.example.demo.service.UserService;
 
@@ -25,11 +28,14 @@ public class UserController {
 	}
 
 	@PostMapping("/register")
-	public String submitForm(@ModelAttribute UserForm form, Model model) {
+	public String submitForm(@ModelAttribute UserForm form, HttpSession session) {
 		// ビジネスロジックを Service に委譲する
 		userService.register(form);
 
-		model.addAttribute("form", form);
-		return "user/result";
+		User loginUser = userService.login(form.getEmail());
+
+		session.setAttribute("loginUser", loginUser);
+
+		return "redirect:/mypage";
 	}
 }
