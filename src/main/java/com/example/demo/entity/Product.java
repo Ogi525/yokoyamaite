0c3a5e22a93226c2ec0cd6d67a0a6e3a65b3d267
Tomoki Honma <homma.tomoki0212@gmail.com>
2026-05-15 09:59:04 +0900

    homeページの内部構造を実装

---
+++ src/main/java/com/example/demo/entity/Product.java
@@ -0,0 +1,89 @@
+package com.example.demo.entity;
+
+public class Product {
+
+	private Integer productId;
+	private Integer areaId;
+	private Integer categoryId;
+
+	private String productName;
+	private Integer price;
+	private String origin;
+	private String imageUrl;
+	private String description;
+	private String sushiLaneName;
+
+	// getter setter
+
+	public Integer getProductId() {
+		return productId;
+	}
+
+	public void setProductId(Integer productId) {
+		this.productId = productId;
+	}
+
+	public Integer getAreaId() {
+		return areaId;
+	}
+
+	public void setAreaId(Integer areaId) {
+		this.areaId = areaId;
+	}
+
+	public Integer getCategoryId() {
+		return categoryId;
+	}
+
+	public void setCategoryId(Integer categoryId) {
+		this.categoryId = categoryId;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public Integer getPrice() {
+		return price;
+	}
+
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+
+	public String getOrigin() {
+		return origin;
+	}
+
+	public void setOrigin(String origin) {
+		this.origin = origin;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getSushiLaneName() {
+		return sushiLaneName;
+	}
+
+	public void setSushiLaneName(String sushiLaneName) {
+		this.sushiLaneName = sushiLaneName;
+	}
+}
