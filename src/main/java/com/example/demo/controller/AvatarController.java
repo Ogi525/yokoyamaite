55244d72136383abbaabb9825bcc2a773acca12f
横山凜 <yyoko02212003@gmail.com>
2026-05-20 09:18:33 +0900

    文字背景が白くなるバグを解消

--- src/main/java/com/example/demo/controller/AvatarController.java
+++ src/main/java/com/example/demo/controller/AvatarController.java
@@ -25,14 +25,9 @@ public class AvatarController {
 		String colorFill = "rgb(" + colorCodeR + "," + colorCodeG + "," + colorCodeB + ")";
 
 		String imageSrc = """
-					<svg xmlns="http://www.w3.org/2000/svg"
-				     width="100"
-				     height="100">
+							<svg xmlns="http://www.w3.org/2000/svg" width="100" height="100">
 
-				    <circle cx="50"
-				            cy="50"
-				            r="50"
-				            fill="%s" />
+				    <circle cx="50" cy="50" r="50" fill="%s" />
 
 				    <text x="50%%"
 				          y="50%%"
@@ -46,7 +41,7 @@ public class AvatarController {
 				    </text>
 
 				</svg>
-				""";
+						""";
 
 		return imageSrc.formatted(colorFill, initial);
 	}
