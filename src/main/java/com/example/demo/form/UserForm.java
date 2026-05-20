fac3602cf311cc3777fd7e5146c4e59b2dc8c17b
wata <watawata.1524@gmail.com>
2026-05-20 14:11:17 +0900

    新規登録画面のバリデーションを追加

--- src/main/java/com/example/demo/form/UserForm.java
+++ src/main/java/com/example/demo/form/UserForm.java
@@ -1,11 +1,21 @@
-package com.example.demo;
+package com.example.demo.form;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 
 public class UserForm {
+	@NotBlank(message = "名前を入力してください")
 	private String name;
+	@NotBlank(message = "メールアドレスを入力してください")
+	@Email(message = "メールアドレスの形式が正しくありません")
 	private String email;
+	@NotBlank(message = "パスワードを入力してください")
 	private String password;
+	@NotBlank(message = "郵便番号を入力してください")
 	private String postal_code;
+	@NotBlank(message = "住所を入力してください")
 	private String address;
+	@NotBlank(message = "パスワードを入力してください")
 	private String confirmPassword;
 
 	public String getConfirmPassword() {
